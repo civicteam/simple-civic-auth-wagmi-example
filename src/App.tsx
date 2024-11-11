eed9e07ac5060684d3e07bdffdb9e338179bee13
Patrick Charrier <patrick.charrier@gmail.com>
2024-11-12 00:00:54 +0100

    remove nextjs dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { WagmiProvider, createConfig, useAccount, useConnect, useSendTransaction, http } from 'wagmi';
 import { CivicAuthProvider, embeddedWallet, NewWeb3User, useUser, Web3User } from '@civic/auth-web3';
-import { userHasWallet } from '@civic/auth-web3/nextjs';
 import { mainnet, sepolia } from "wagmi/chains";
 
 const CLIENT_ID = process.env.CLIENT_ID;
 if (!CLIENT_ID) throw new Error('CLIENT_ID is required');
+
+const userHasWallet = (user: any) => (user.user as Web3User).walletAddress;
 
 const wagmiConfig = createConfig({
   chains: [ mainnet, sepolia ],
@@ -23,7 +24,7 @@ const queryClient = new QueryClient();
 
 const App = () => {
   // the civic user hook
-  const { user } = useUser();
+  const user = useUser();
 
   // add the wagmi hooks
   const { connect, connectors } = useConnect();
@@ -32,10 +33,10 @@ const App = () => {
 
   // A function that creates the wallet if the user doesn't have one already
   const createWallet = () => {
-    if (user && !userHasWallet((user as Web3User))) {
+    if (user && !userHasWallet(user)) {
       // once the wallet is created, we can connect it straight away
-      if((user as NewWeb3User).createWallet) {
-        return (user as NewWeb3User).createWallet!().then(connectExistingWallet);
+      if((user.user as NewWeb3User).createWallet) {
+        return (user.user as NewWeb3User).createWallet!().then(connectExistingWallet);
       };
     }
   }
@@ -53,7 +54,7 @@ const App = () => {
       <QueryClientProvider client={queryClient}>
         <WagmiProvider config={wagmiConfig}>
           <CivicAuthProvider>
-            { user && !userHasWallet &&
+            { user && !userHasWallet(user) &&
                 <button onClick={createWallet}>Create Wallet</button>
             }
             {!isConnected ? (
