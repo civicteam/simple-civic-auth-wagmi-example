b79cbe437201e88e1aad87c9004fc3c9a848eecb
lucmir <luccmir@gmail.com>
2024-11-27 16:24:15 -0300

    Fix reading client ID from env var

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,6 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    "process.env": {
-      CLIENT_ID: "my-client",
-    },
-  },
   server: {
     port: 3000,
   },
